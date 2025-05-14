8e86a37a3d69e5517fd3ab8b93bbd0ca918109e1
Átila <144128328+atilaF03@users.noreply.github.com>
2025-05-14 07:01:19 -0300

    Add files via upload

---
+++ br.com.exemplo_Atividade_jar_1.0-SNAPSHOT/src/main/java/br/com/exemplo/atividade/Atividade.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.exemplo.atividade;
+
+import Telas.Login;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Atividade {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+}
---
+++ br.com.exemplo_Atividade_jar_1.0-SNAPSHOT/src/main/java/br/com/exemplo/atividade/Usuario.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.exemplo.atividade;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Usuario {
+    private String login;
+    private String senha;
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
